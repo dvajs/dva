0e2171086ef0e179c2db68303d872b55987263c7
sorrycc <sorrycc@gmail.com>
2016-08-06 13:32:36 +0800

    update example, use babel-plugin-dva-hmr

--- examples/user-dashboard/src/index.js
+++ examples/user-dashboard/src/index.js
@@ -9,42 +9,11 @@ const app = dva({
   },
 });
 
-// 2. Plugins
-app.use(hmr());
-
-// 3. Model
+// 2. Model
 app.model(require('./models/users'));
 
-// 4. Router
+// 3. Router
 app.router(require('./router'));
 
-// 5. Start
+// 4. Start
 app.start(document.getElementById('root'));
-
-// Hmr helper
-// This will be move to babel plugin in later versions.
-function hmr() {
-  return {
-    onHmr(render) {
-      if (module.hot) {
-        const renderNormally = render;
-        const renderException = (error) => {
-          const RedBox = require('redbox-react');
-          ReactDOM.render(<RedBox error={error} />, document.etElementById('root'));
-        };
-        const newRender = (router) => {
-          try {
-            renderNormally(router);
-          } catch (error) {
-            console.error('error', error);
-            renderException(error);
-          }
-        };
-        module.hot.accept('./router', () => {
-          const router = require('./router');
-          newRender(router);
-        });
-      }
-    },
-  };
-}
--- examples/user-dashboard/webpack.config.js
+++ examples/user-dashboard/webpack.config.js
@@ -2,11 +2,20 @@ const fs = require('fs');
 const path = require('path');
 const glob = require('glob');
 
-module.exports = function (webpackConfig) {
+module.exports = function (webpackConfig, env) {
   webpackConfig.babel.plugins.push('transform-runtime');
   webpackConfig.babel.plugins.push(['antd', {
     style: 'css'  // if true, use less
   }]);
+
+  // Enable hmr for development.
+  if (env === 'development') {
+    webpackConfig.babel.plugins.push(['dva-hmr', {
+      entries: [
+        './src/index.js',
+      ],
+    }]);
+  }
 
   // Parse all less files as css module.
   webpackConfig.module.loaders.forEach(function (loader, index) {
