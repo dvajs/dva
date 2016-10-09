5e85fb2f839e0b1842c795c7e98db5028148f5cc
nikogu <644506165@qq.com>
2016-10-09 17:56:28 +0800

    indent 4->2

--- examples/count-ts/src/index.tsx
+++ examples/count-ts/src/index.tsx
@@ -13,36 +13,36 @@ const app = dva();
 
 // 2. Model
 app.model({
-    namespace: 'count',
-    state: 0,
-    reducers: {
-        add  (count) {
-            return count + 1
-        },
-        minus(count) {
-            return count - 1
-        },
+  namespace: 'count',
+  state: 0,
+  reducers: {
+    add  (count) {
+      return count + 1
     },
+    minus(count) {
+      return count - 1
+    },
+  },
 });
 
 // 3. View
 const App = connect(({ count }) => ({
-    count
+  count
 }))(function (props) {
-    return (
-        <div>
-            <h2>{ props.count }</h2>
-            <button key="add" onClick={() => { props.dispatch({type: 'count/add'})}}>+</button>
-            <button key="minus" onClick={() => { props.dispatch({type: 'count/minus'})}}>-</button>
-        </div>
-    );
+  return (
+    <div>
+      <h2>{ props.count }</h2>
+      <button key="add" onClick={() => { props.dispatch({type: 'count/add'})}}>+</button>
+      <button key="minus" onClick={() => { props.dispatch({type: 'count/minus'})}}>-</button>
+    </div>
+  );
 });
 
 // 4. Router
 app.router(({ history }) =>
-    <Router history={history}>
-        <Route path="/" component={App}/>
-    </Router>
+  <Router history={history}>
+    <Route path="/" component={App}/>
+  </Router>
 );
 
 // 5. Start
