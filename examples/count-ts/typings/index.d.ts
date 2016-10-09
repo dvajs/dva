9c1175689da7b336bf60bd0f88f05aacf9464447
nikogu <644506165@qq.com>
2016-10-09 17:28:27 +0800

    add index.d.ts & typescript demo

---
+++ examples/count-ts/typings/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="globals/antd/index.d.ts" />
+/// <reference path="globals/dva/index.d.ts" />
+/// <reference path="globals/react-dom/index.d.ts" />
+/// <reference path="globals/react/index.d.ts" />
---
+++ examples/count-ts/webpack.config.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+const glob = require('glob');
+
+module.exports = function (webpackConfig, env) {
+  webpackConfig.babel.plugins.push('transform-runtime');
+  webpackConfig.babel.plugins.push(['antd', {
+    style: 'css'  // if true, use less
+  }]);
+
+  // Enable hmr for development.
+  if (env === 'development') {
+    webpackConfig.babel.plugins.push(['dva-hmr', {
+      entries: [
+        './src/index.js',
+      ],
+    }]);
+  }
+
+  // Parse all less files as css module.
+  webpackConfig.module.loaders.forEach(function (loader, index) {
+    if (typeof loader.test === 'function' && loader.test.toString().indexOf('\\.less$') > -1) {
+      loader.test = /\.dont\.exist\.file/;
+    }
+    if (loader.test.toString() === '/\\.module\\.less$/') {
+      loader.test = /\.less$/;
+    }
+  });
+
+  return webpackConfig;
+};
