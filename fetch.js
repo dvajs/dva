42b0b2ccd4113f2caae652683809a8815c3e377e
sorrycc <sorrycc@gmail.com>
2016-10-18 11:37:54 +0800

    feat: use isomorphic-fetch, Close #162

--- fetch.js
+++ fetch.js
@@ -1,6 +1,2 @@
-require('whatwg-fetch');
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = self.fetch.bind(self);
+module.exports = require('isomorphic-fetch');
