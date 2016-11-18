799f8e8445d4f2fe648bea126dd4266377db40c6
sorrycc <sorrycc@gmail.com>
2016-11-19 06:48:17 +0800

    examples: change user-dashboard to browserHistory, ref #288

--- examples/user-dashboard/src/index.js
+++ examples/user-dashboard/src/index.js
@@ -1,9 +1,12 @@
 import './index.html';
 import './index.less';
 import dva from 'dva';
+import { browserHistory } from 'dva/router';
 
 // 1. Initialize
-const app = dva();
+const app = dva({
+  history: browserHistory,
+});
 
 // 2. Model
 app.model(require('./models/users'));
