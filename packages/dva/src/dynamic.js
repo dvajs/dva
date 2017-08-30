3e36a01753f2b094c41859841438af0ad675c41e
chencheng <sorrycc@gmail.com>
2017-08-30 18:00:39 +0800

    add dynamic util

---
+++ packages/dva/src/dynamic.js
@@ -0,0 +1,29 @@
+import { asyncComponent } from 'react-async-component';
+
+const cached = {};
+function registerModel(app, model) {
+  if (!cached[model.namespace]) {
+    app.model(model);
+    cached[model.namespace] = 1;
+  }
+}
+
+export default function({ app, models, component }) {
+  return asyncComponent({
+    resolve() {
+      return new Promise((resolve) => {
+        Promise.all([...(models||[]), component]).then((ret) => {
+          if (!models || !models.length) {
+            return resolve(ret[0]);
+          } else {
+            const len = models.length;
+            ret.slice(0, len).forEach((m) => {
+              registerModel(app, m);
+            });
+            resolve(ret[len]);
+          }
+        });
+      });
+    },
+  });
+}
