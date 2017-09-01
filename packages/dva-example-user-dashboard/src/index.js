813685f7b9f39c6bbe8e9e22f23679af871083f7
chencheng <sorrycc@gmail.com>
2017-09-01 16:23:26 +0800

    example: clean

--- packages/dva-example-user-dashboard/src/index.js
+++ packages/dva-example-user-dashboard/src/index.js
@@ -2,7 +2,6 @@ import dva from 'dva';
 import { browserHistory } from 'dva/router';
 import createLoading from 'dva-loading';
 import { message } from 'antd';
-import './index.html';
 import './index.css';
 
 const ERROR_MSG_DURATION = 3; // 3 秒
