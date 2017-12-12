fd32ee67e7b5263510fff38457e1485e1206e25b
chencheng <sorrycc@gmail.com>
2017-12-12 17:47:38 +0800

    add d.ts for typescript, Close #1248

---
+++ packages/dva/dynamic.d.ts
@@ -0,0 +1,2 @@
+declare const dynamic: (resolve: (value?: PromiseLike<any>) => void) => void;
+export default dynamic;
---
+++ packages/dva/router.d.ts
@@ -0,0 +1,4 @@
+import * as routerRedux from 'react-router-redux';
+
+export * from 'react-router';
+export { routerRedux };
