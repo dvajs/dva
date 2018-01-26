bdc6285cccefc9f0dd8e77504748c777080ceacb
Henry <henry.ding@3vyd.com>
2018-01-26 16:15:30 +0800

    重写判断条件，看着更清楚一点

--- packages/dva/src/index.js
+++ packages/dva/src/index.js
@@ -50,12 +50,14 @@ export default function (opts = {}) {
         `[app.start] container ${container} not found`,
       );
     }
-
+    // 如果container不为空
     // 并且是 HTMLElement
-    invariant(
-      isHTMLElement(container),
-      `[app.start] container should be HTMLElement`,
-    );
+    if (!container) {
+      invariant(
+        isHTMLElement(container),
+        `[app.start] container should be HTMLElement`,
+      );
+    }
 
     // 路由必须提前注册
     invariant(
