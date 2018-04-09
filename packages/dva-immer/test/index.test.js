1868cd754f1b788c6b92d5f6825d6e85b24a2520
chencheng <sorrycc@gmail.com>
2018-04-09 16:50:47 +0800

    test: add testcase for dva-immer

--- packages/dva-immer/test/index.test.js
+++ packages/dva-immer/test/index.test.js
@@ -2,26 +2,32 @@ import dva from 'dva';
 import userImmer from '../src/index';
 
 describe('dva-immer', () => {
-  xit('normal', () => {
-    const app = dva.default();
+  it('normal', () => {
+    const app = dva();
     app.use(userImmer());
 
     app.model({
       namespace: 'count',
       state: {
-        count: 111,
-        text: 'hi',
+        a: {
+          b: {
+            c: 0,
+          },
+        },
       },
       reducers: {
         add(state) {
-          state.count += 1;
+          state.a.b.c += 1;
         },
       },
     });
     app.router(() => 1);
     app.start();
 
+    const oldCount = app._store.getState().count;
     app._store.dispatch({ type: 'count/add' });
-    console.log(app._store.getState().count);
+    const newCount = app._store.getState().count;
+    expect(oldCount.a.b.c).toEqual(0);
+    expect(newCount.a.b.c).toEqual(1);
   });
 });
