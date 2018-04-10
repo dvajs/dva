81c9f786966a642463e0f8efa2f3a7f577ee9343
chencheng <sorrycc@gmail.com>
2018-04-10 17:27:25 +0800

    fix: entry for dva-no-router and dva-react-router-3

--- packages/dva-no-router/index.js
+++ packages/dva-no-router/index.js
@@ -1,6 +1,2 @@
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
-exports.default = require('./lib').default;
-exports.connect = require('react-redux').connect;
+module.exports = require('./lib');
+module.exports.connect = require('react-redux').connect;
