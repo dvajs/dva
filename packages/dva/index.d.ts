a345407035768a5404ef2652d311a73ed52e6efb
alwaysonlinetxm <alwaysonlinetxm@163.com>
2018-04-19 12:24:55 +0800

    fix the type of router in DvaInstance

--- packages/dva/index.d.ts
+++ packages/dva/index.d.ts
@@ -44,9 +44,10 @@ export interface EffectsCommandMap {
 
 export type Effect = (action: Action, effects: EffectsCommandMap) => void;
 export type EffectType = 'takeEvery' | 'takeLatest' | 'watcher' | 'throttle';
-export type EffectWithType = [Effect, { type : EffectType }];
+export type EffectWithType = [Effect, { type: EffectType }];
 export type Subscription = (api: SubscriptionAPI, done: Function) => void;
 export type ReducersMapObjectWithEnhancer = [ReducersMapObject, ReducerEnhancer];
+export type RouterGetter = (api: RouterAPI) => JSX.Element | Object;
 
 export interface EffectsMapObject {
   [key: string]: Effect | EffectWithType;
@@ -107,7 +108,7 @@ export interface DvaInstance {
    *
    * @param router
    */
-  router: (router: Router) => void,
+  router: (router: RouterGetter) => void,
 
   /**
    * Start the application. Selector is optional. If no selector
