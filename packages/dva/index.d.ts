fdc64b2144a7a611de23a5e264347d1f2e080778
baurine <2008.hbl@gmail.com>
2018-08-07 18:01:02 +0800

    chore: unify the ending symbol for interface properties
    - some places use `,`, some places use `;`, unify them by `,` to keep consistence

--- packages/dva/index.d.ts
+++ packages/dva/index.d.ts
@@ -10,36 +10,36 @@ import {
 import { History } from "history";
 
 export interface onActionFunc {
-  (api: MiddlewareAPI<any>): void;
+  (api: MiddlewareAPI<any>): void,
 }
 
 export interface ReducerEnhancer {
-  (reducer: Reducer<any>): void
+  (reducer: Reducer<any>): void,
 }
 
 export interface Hooks {
-  onError?: (e: Error, dispatch: Dispatch<any>) => void;
-  onAction?: onActionFunc | onActionFunc[];
-  onStateChange?: () => void;
-  onReducer?: ReducerEnhancer;
-  onEffect?: () => void;
-  onHmr?: () => void;
-  extraReducers?: ReducersMapObject;
-  extraEnhancers?: StoreEnhancer<any>[];
+  onError?: (e: Error, dispatch: Dispatch<any>) => void,
+  onAction?: onActionFunc | onActionFunc[],
+  onStateChange?: () => void,
+  onReducer?: ReducerEnhancer,
+  onEffect?: () => void,
+  onHmr?: () => void,
+  extraReducers?: ReducersMapObject,
+  extraEnhancers?: StoreEnhancer<any>[],
 }
 
 export type DvaOption = Hooks & {
-  initialState?: Object;
-  history?: Object;
+  initialState?: Object,
+  history?: Object,
 }
 
 export interface EffectsCommandMap {
-  put: <A extends AnyAction>(action: A) => any;
-  call: Function;
-  select: Function;
-  take: Function;
-  cancel: Function;
-  [key: string]: any;
+  put: <A extends AnyAction>(action: A) => any,
+  call: Function,
+  select: Function,
+  take: Function,
+  cancel: Function,
+  [key: string]: any,
 }
 
 export type Effect = (action: AnyAction, effects: EffectsCommandMap) => void;
@@ -49,16 +49,16 @@ export type Subscription = (api: SubscriptionAPI, done: Function) => void;
 export type ReducersMapObjectWithEnhancer = [ReducersMapObject, ReducerEnhancer];
 
 export interface EffectsMapObject {
-  [key: string]: Effect | EffectWithType;
+  [key: string]: Effect | EffectWithType,
 }
 
 export interface SubscriptionAPI {
-  history: History;
-  dispatch: Dispatch<any>;
+  history: History,
+  dispatch: Dispatch<any>,
 }
 
 export interface SubscriptionsMapObject {
-  [key: string]: Subscription;
+  [key: string]: Subscription,
 }
 
 export interface Model {
@@ -70,12 +70,12 @@ export interface Model {
 }
 
 export interface RouterAPI {
-  history: History;
-  app: DvaInstance;
+  history: History,
+  app: DvaInstance,
 }
 
 export interface Router {
-  (api?: RouterAPI): JSX.Element | Object;
+  (api?: RouterAPI): JSX.Element | Object,
 }
 
 export interface DvaInstance {
