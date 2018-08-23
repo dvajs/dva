fbba95c581a8176b4c7c04ae10a1acb8ba93393a
Min <dicklwm@163.com>
2018-08-24 00:07:53 +0800

    fix: rewrite dynamic.d.ts
    #1758， 修复此问题

--- packages/dva/dynamic.d.ts
+++ packages/dva/dynamic.d.ts
@@ -1,2 +1,9 @@
-declare const dynamic: (resolve: (value?: PromiseLike<any>) => void) => void;
+import { ComponentType } from 'react';
+import { DvaInstance } from './index';
+
+declare const dynamic: (opts: {
+  app: DvaInstance,
+  models?: () => PromiseLike<any>[],
+  component: () => PromiseLike<any>,
+}) => ComponentType<any>;
 export default dynamic;
