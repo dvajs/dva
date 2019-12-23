94d0d3a438b5b16428378ac72a1fa646e7a59d35
becarchal <532477934@qq.com>
2019-12-23 10:58:39 +0800

    fix: effects puting reducers does not work if reducers is array (#2265)
    * fix the bug that effects puting reducers does not work if reducers is array
    
    * fix prefixType

--- packages/dva-core/src/prefixType.js
+++ packages/dva-core/src/prefixType.js
@@ -3,10 +3,11 @@ import { NAMESPACE_SEP } from './constants';
 export default function prefixType(type, model) {
   const prefixedType = `${model.namespace}${NAMESPACE_SEP}${type}`;
   const typeWithoutAffix = prefixedType.replace(/\/@@[^/]+?$/, '');
-  if (
-    (model.reducers && model.reducers[typeWithoutAffix]) ||
-    (model.effects && model.effects[typeWithoutAffix])
-  ) {
+
+  const reducer = Array.isArray(model.reducers)
+    ? model.reducers[0][typeWithoutAffix]
+    : model.reducers && model.reducers[typeWithoutAffix];
+  if (reducer || (model.effects && model.effects[typeWithoutAffix])) {
     return prefixedType;
   }
   return type;
