36998cd911b2f9e7f3c007ff13b53d499f8fa47e
杨斌 <Bin2.Yang@five-star.cn>
2020-01-01 18:03:58 +0800

    Improve the dynamic.d.ts，thanks for zhangyalong`s commit

--- packages/dva/dynamic.d.ts
+++ packages/dva/dynamic.d.ts
@@ -1,9 +1,22 @@
-interface Idynamic {
-  app: any, models?: any, component: any,
-  resolve?:(value?: PromiseLike<any>) => void,
+// interface Idynamic {
+//   app: any, models?: any, component: any,
+//   resolve?:(value?: PromiseLike<any>) => void,
+// }
+
+import { DvaInstance } from 'dva';
+import React from 'react';
+
+interface Config {
+  app: DvaInstance;
+  component: () => PromiseLike<any>;
+  models?: () => PromiseLike<any>[];
+  resolve?: () => PromiseLike<any>;
+  LoadingComponent?: React.ComponentType;
 }
-declare function dynamic(config: Idynamic): any;
+
+declare function dynamic(config: Config): any;
 declare namespace dynamic {
-  function setDefaultLoadingComponent(LoadingComponent: any): void;
+  function setDefaultLoadingComponent(LoadingComponent: React.ComponentType): void;
 }
+
 export default dynamic;
